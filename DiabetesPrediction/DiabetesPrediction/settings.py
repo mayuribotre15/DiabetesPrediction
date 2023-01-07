d20b09a8e2cc326515e702719fb16a3989493502
Mayuri Botre <mayuribotre1501@gmail.com>
2023-01-07 12:17:59 +0530

    Procfile, runtime and setting files added

--- DiabetesPrediction/DiabetesPrediction/settings.py
+++ DiabetesPrediction/DiabetesPrediction/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-jx%#4&j(@$iclq5g-=)6owtup2r5%zg)fu6wl^*mwu*9#-y6m5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
